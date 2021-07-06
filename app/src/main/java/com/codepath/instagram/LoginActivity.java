fc74e6abf435692ca25f078f781576b0e4d05d1d
tessvandaele <65978033+tessvandaele@users.noreply.github.com>
2021-07-06 16:53:26 -0500

    User can log in and user is persisted across app restarts

--- app/src/main/java/com/codepath/instagram/LoginActivity.java
+++ app/src/main/java/com/codepath/instagram/LoginActivity.java
@@ -2,11 +2,17 @@ package com.codepath.instagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 /* Login Screen */
 
@@ -24,6 +30,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //check if user is already logged in
+        if(ParseUser.getCurrentUser() != null){
+            goMainActivity();
+        }
+
         //assigning views
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
@@ -33,7 +44,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "logging in");
+                //Log.d(TAG, "logging in");
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 loginUser(username, password);
@@ -43,6 +54,26 @@ public class LoginActivity extends AppCompatActivity {
 
     //navigate to main activity if user passes validation
     private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user " + username + " with password " +password);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null) { //exception occured
+                    Toast.makeText(LoginActivity.this, "username or password incorrect", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "login unsuccessful", e);
+                    return;
+                }
 
+                //no exception; navigate to main activity
+                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                goMainActivity();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
---
+++ app/src/main/java/com/codepath/instagram/ParseApplication.java
@@ -0,0 +1,23 @@
+package com.codepath.instagram;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //register our parse models
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("Ymb4uHNa3PekdrJtTx9lwOdZeTMIpQYs6tDI2kGP")
+                .clientKey("vDw3U7CejAV9Rhwuru3A78E3CpTB8bh20Hd11PRF")
+                .server("https://parseapi.back4app.com")
+                .build());
+    }
+}
